44f643358aad443122f2905b63291d474b92225d
terriera <alan.terrier@etu.umontpellier.fr>
2023-12-08 04:54:00 +0100

    initialisation d'arbre

--- ressources/test/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.querySelector('.menu-icon');
-    const nav = document.querySelector('nav');
-
-    menuIcon.addEventListener('click', function () {
-        nav.style.display = (nav.style.display === 'block' ? 'none' : 'block');
-    });
-});
